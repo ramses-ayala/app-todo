90186be78d4801dcda6b82f86f81850326a9c0d6
ramses-ayala <ramses0595@gmail.com>
2023-09-11 18:37:10 -0500

    adjustments related to the routes

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+
+export interface IDashboardProps{}
+
+const Dashboard: React.FC<IDashboardProps> = (props) => {
+    return(
+        <h1>Dashboard component</h1>
+    )
+}
+
+export default Dashboard;
