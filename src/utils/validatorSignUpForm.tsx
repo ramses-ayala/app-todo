31180194507342ee3f68cdafd51cb966c5217297
ramses-ayala <ramses0595@gmail.com>
2023-10-23 13:53:22 -0500

    check if an email is already registered functionality ready

--- src/utils/validatorSignUpForm.tsx
+++ src/utils/validatorSignUpForm.tsx
@@ -1,8 +1,10 @@
+import axios from "axios";
+
 import FormInputs from "../interfaces/FormInputs"; 
 
 import { checkErrors } from "./checkErrors";
 
-const validatorSignUpForm = (values: FormInputs): Object => {
+const validatorSignUpForm = async(values: FormInputs): Promise<Object> => {
 
     let errors: FormInputs = {
         email: "",
@@ -22,30 +24,31 @@ const validatorSignUpForm = (values: FormInputs): Object => {
     }
 
 
+
     if (!values.email) {
         errors.email = 'Email required !!!';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{3}$/i.test(values.email)) {
         errors.email = 'Invalid email address';
-    }
+    }else{        
+        
+        const onlyCheckingEmail = true;
 
+        const resp = await axios.post(`http://localhost:8000/api/signUp/${onlyCheckingEmail}`, {
+            'email': values.email
+        })
+        
+        if(resp.data.emailExists !== false){
+            errors.email = 'Email is already registered !!!';
+        }else{
+            errors.email = '';
+        }
+    
+    }
 
     if (!values.password) {
         errors.password = 'Password required !!!';
     }
     else if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{10,})/.test(values.password)) {
-        errors.password = 'Your password must have at least 10 characters, 1 uppercase, 1 lowercase and 1 numeric.';
-    }
-    
-
-    if (!values.email) {
-        errors.email = 'Email required !!!';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address';
-    }
-
-    if (!values.password) {
-        errors.password = 'Password required !!!';
-    } else if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{10,})/.test(values.password)) {
         errors.password = 'Your password must have at least 10 characters, 1 uppercase, 1 lowercase and 1 numeric.';
     }    
 
